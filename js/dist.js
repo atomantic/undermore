2726887eae037b8b06b49f06414c93de4456db93
Adam Eivy <atomantic@gmail.com>
2014-04-23 22:31:25 +0000

    adding the yeoman build files to gh-pages [ci skip]

--- js/dist.js
+++ js/dist.js
@@ -1,42 +1,53 @@
 /*global DownloadBuilder*/
 
-(function(){
+(function() {
 
-	var $toggler = $('.toggle-all'),
-		$jsDownload = $("#javascript-download");
+    var $toggler = $('.toggle-all'),
+        $jsDownload = $("#javascript-download"),
+        $section = $toggler.closest('section'),
+        $checkboxes = $section.find('input[type=checkbox]').not(":disabled"),
+        $es6 = $('.es6-shim'),
+        $inES6 = $checkboxes.filter('.in-es6-shim');
 
-
-	$("a[href='#']").click(function(event) {
+    $("a[href='#']").click(function(event) {
         event.preventDefault();
     });
 
-	$toggler.click(function(){
-		var on = $toggler.data('on');
-		$toggler.closest('section').find('input[type=checkbox]').not(":disabled").prop('checked', on ? 'checked' : false);
-		$toggler.data('on',!on);
-	});
+    $es6.click(function() {
+        $inES6.prop('checked', $(this).is(':checked') ? false : 'checked');
+    });
 
-    var builder = new DownloadBuilder({ 
-      "location": "github", 
-      "author": "atomantic", 
-      "repo": "undermore",
-      "branch": "dev",
-      "onError": function(error){
-          $('#errorMessage').html(error).show();
-      }
+    $toggler.click(function() {
+        var on = $toggler.data('on');
+        $checkboxes.prop('checked', on ? 'checked' : false);
+        if (on) {
+            $inES6.prop('checked', false);
+        }
+        $toggler.data('on', !on);
+    });
+
+    var builder = new DownloadBuilder({
+        "location": "github",
+        "author": "atomantic",
+        "repo": "undermore",
+        "branch": "dev",
+        "onError": function(error) {
+            $('#errorMessage').html(error).show();
+        }
     });
 
     $("#javascript-generate").on("click", function() {
         builder.buildURL(
-        	$("#javascript-downloads input[type='checkbox']:checked"), "undermore.js", "javascript", function(data) {
-            $("#generated-javascript-source").text(data.content);
-            if(!data.content) {
-                $jsDownload.fadeOut("slow");
-            }
-            if(data.url) {
-                $jsDownload.attr("href", data.url).fadeIn("slow");
+            $("#javascript-downloads input[type='checkbox']:checked"), "undermore.js", "javascript", function(data) {
+                $("#generated-javascript-source").text(data.content);
+                if (!data.content) {
+                    $jsDownload.fadeOut("slow");
+                }
+                if (data.url) {
+                    $jsDownload.attr("href", data.url).fadeIn("slow");
 
+                }
             }
-        });
+        );
     });
 })();
